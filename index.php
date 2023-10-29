e0308298198d8226e205419352004a9b15060d3d
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2023-10-28 23:53:17 -0500

    SALOME CANSONA

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
                 <p>
                     Somos un grupo de personas que desarrollaron este banco para facilitar la publicación y la búsqueda de los personales necesitados y empleos en la localidad cuarta de San Cristóbal
                 </p>
-                 <a href="" class="btn-1">Mas información</a> 
+                 <a href="https://youtu.be/iPT_rjltqh0?si=ghRLw4ZxL-UEgAzi" class="btn-1">Mas información</a> 
             </div>
             <img src="IMG/astronauta.png" alt="">
         </div>
