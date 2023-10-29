4e831309b0067301c6c9a74008445edf73122608
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2023-10-28 21:24:18 -0500

    Htp Vida Se Me Olvidaaaaaa

--- CRUD-MODAL/index.php
+++ CRUD-MODAL/index.php
@@ -30,7 +30,7 @@ $dir = "posters/";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Modal</title>
+    <title>CRUD WEENCLOUD</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/all.min.css" rel="stylesheet">
