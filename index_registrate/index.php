e10e98f7846b4305a072b6d748644d66ff5a07e8
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2023-10-31 18:37:16 -0500

    ajaaaa

--- index_registrate/index.php
+++ index_registrate/index.php
@@ -6,7 +6,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="estilos.css">
     <title>Ween Cloud</title>
--- index_registrate/index2.php
+++ index_registrate/index2.php
@@ -17,7 +17,6 @@
                             <div class="info-childs">
                                 <h1>¡¡Bienvenido a Ween Cloud!!</h1>
                                 <p>Para buscar o solicitar un empleo porfavor inicia sesion o registrate con tus datos</p>
-                                <!-- <input type="button" value="Registrarse" id="sign-up"> -->
                             </div>
                         </div>
                          <div class="form-information">
