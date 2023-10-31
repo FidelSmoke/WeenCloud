49ccac69b1e9da8e8e2fe923976d3448c1a2e352
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2023-10-31 06:45:33 -0500

    SOCIOO HAGA ALGO

--- CRUD-MODAL/index.php
+++ CRUD-MODAL/index.php
@@ -18,7 +18,7 @@ $dir = "posters/";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD WEENCLOUD</title>
+    <title>WEENCLOUD</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/all.min.css" rel="stylesheet">
